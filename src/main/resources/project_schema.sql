c81d65d5ee509b6f5df813b0c0c55da102928128
[Corinne Padilla] <[corinnepadilla@yahoo.com]>
2024-10-11 14:49:06 -0700

    modified DaoBase and projectSchema

--- src/main/resources/project_schema.sql
+++ src/main/resources/project_schema.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS material;
 DROP TABLE IF EXISTS project;
 
 CREATE TABLE project (
-  project_id INT AUTO_INCREMENT NOT NULL,
+  project_id INT  NOT NULL AUTO_INCREMENT,
   project_name VARCHAR(128) NOT NULL,
   estimated_hours DECIMAL(7,2),
   actual_hours DECIMAL(7,2),
